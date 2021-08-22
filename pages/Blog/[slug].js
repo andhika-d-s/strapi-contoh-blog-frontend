edf489dfd8fd4348e3f0e045accc32e3c16668a5
andhika-d-s <andhika.dwis02@gmail.com>
2021-08-22 20:45:13 +0800

    perbaikan lagi fix kedua

--- pages/Blog/[slug].js
+++ pages/Blog/[slug].js
@@ -1,61 +1,59 @@
 import Markdown from 'markdown-to-jsx'
 
-export default function Post({ viewPost }) {
-    // const {published_at} = viewPost
-    // const date = new Date(published_at)
-    // const formatDate = new Intl.DateTimeFormat('id', { dateStyle: 'full' }).format(date)
-    // const author = viewPost.users_permissions_user.username
-    // const image = `https://strapi-blog-contoh.herokuapp.com${viewPost.thumbnail_img.url}`
-    // return(
-    //     <div className="container"> 
-    //         <div className="flex flex-col px-8 xl:px-24">
-    //             <div className="mt-28 text-gray-400 flex flex-row justify-between mb-2">
-    //                 <p>{formatDate}</p>
-    //                 <p>{author}</p>
-    //             </div>
-    //             <div className="pb-3">
-    //                 <h1 className="font-bold text-4xl lg:text-6xl">{viewPost.titles}</h1>
-    //             </div>
-    //             <div className="text-center mb-2">
-    //                 <div className="bg-cover bg-no-repeat h-96 2xl:h-96 rounded-md" style={{ backgroundImage: `url(${image})` }}></div>
-    //                 <p className="text-gray-400 py-3">{viewPost.textThumbnail}</p>
-    //             </div>
-    //             <div className="flex flex-col px-2 leading-7">
-    //                 <Markdown options={{
-    //                     wrapper: 'article',
-    //                     overrides: {
-    //                         a: {
-    //                             props: {
-    //                                 className: 'underline'
-    //                             }
-    //                         },
-    //                         ol: {
-    //                             props: {
-    //                                 className: 'list-decimal pl-5'
-    //                             }
-    //                         },
-    //                         strong: {
-    //                             props: {
-    //                                 className: 'text-xl'
-    //                             }
-    //                         },
-    //                         ul: {
-    //                             props: {
-    //                                 className: 'pl-5 list-disc'
-    //                             }
-    //                         },
-    //                         h3: {
-    //                             props: {
-    //                                 className: 'text-3xl'
-    //                             }
-    //                         }
-    //                     }
-    //                 }} >{viewPost.content}</Markdown>
-    //             </div>
-    //         </div>
-    //     </div>
-    // )
-    return 0
+export default function Post({ viewPost: {users_permissions_user: {username}, published_at} }) {
+    const date = new Date(published_at)
+    const formatDate = new Intl.DateTimeFormat('id', { dateStyle: 'full' }).format(date)
+    const author = username
+    const image = `https://strapi-blog-contoh.herokuapp.com${viewPost.thumbnail_img.url}`
+    return(
+        <div className="container"> 
+            <div className="flex flex-col px-8 xl:px-24">
+                <div className="mt-28 text-gray-400 flex flex-row justify-between mb-2">
+                    <p>{formatDate}</p>
+                    <p>{author}</p>
+                </div>
+                <div className="pb-3">
+                    <h1 className="font-bold text-4xl lg:text-6xl">{viewPost.titles}</h1>
+                </div>
+                <div className="text-center mb-2">
+                    <div className="bg-cover bg-no-repeat h-96 2xl:h-96 rounded-md" style={{ backgroundImage: `url(${image})` }}></div>
+                    <p className="text-gray-400 py-3">{viewPost.textThumbnail}</p>
+                </div>
+                <div className="flex flex-col px-2 leading-7">
+                    <Markdown options={{
+                        wrapper: 'article',
+                        overrides: {
+                            a: {
+                                props: {
+                                    className: 'underline'
+                                }
+                            },
+                            ol: {
+                                props: {
+                                    className: 'list-decimal pl-5'
+                                }
+                            },
+                            strong: {
+                                props: {
+                                    className: 'text-xl'
+                                }
+                            },
+                            ul: {
+                                props: {
+                                    className: 'pl-5 list-disc'
+                                }
+                            },
+                            h3: {
+                                props: {
+                                    className: 'text-3xl'
+                                }
+                            }
+                        }
+                    }} >{viewPost.content}</Markdown>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 // get path blog post with static generation
