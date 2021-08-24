5d28dd05da99328e76da16761a62d3a9a0758c3c
Andhika Dwi Suryanata <73742961+andhika-d-s@users.noreply.github.com>
2021-08-24 20:55:00 +0800

    Update PrimaryButton.js

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 const PrimaryButton = ({children, color}) => {
     return (
-        <div className="py-5 text-xl">
-            <button className={`font-bold transition-colors border-transparent ${color} border-b-4 px-1`}>
-                {children}
-            </button>
-        </div>
+        <a className={`my-5 text-xl font-bold transition-colors border-transparent ${color} border-b-4 px-1`}>
+            {children}
+        </a>
     )
 }
 
