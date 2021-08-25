7e47858fdb7203a96e9ad85cca5ed0a4f3f0794d
andhika-d-s <andhika.dwis02@gmail.com>
2021-08-25 20:10:12 +0800

    fix primaryButton again

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -3,11 +3,13 @@ import Link from 'next/link'
 
 const PrimaryButton = ({children, color, link}) => {
     return (
-        <Link href={link} className="py-5 text-xl">
-            <a className={`font-bold transition-colors border-transparent ${color} border-b-4 px-1`}>
-                {children}
-            </a>
-        </Link>
+        <div className="py-5 ">
+            <Link href={link}>
+                <a className={`cursor-pointer text-xl font-bold transition-colors border-transparent ${color} border-b-4`}>
+                    {children}
+                </a>
+            </Link>
+        </div>
     )
 }
 
